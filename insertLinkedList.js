7a23772f556f227fa9320135d9b83ff5d0af7fb6
Tobias <juantobiasdl7@gmail.com>
2024-03-07 23:06:40 -0600

    I start Algo to reverse a singly linked list.

--- insertLinkedList.js
+++ insertLinkedList.js
@@ -29,20 +29,58 @@ let deleteNode = node => {
     nextNode.prev = prevNode;
 }
 
+
+let deleteNodeModifies = node => {
+    // let prevNode = node.prev;
+    // let nextNode = node.next;
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+}
+
 // Create the nodes
+const node0 = new Node(0);
 const node1 = new Node(1);
 const node2 = new Node(2);
 const node3 = new Node(3);
-const nodeK = new Node("K");
 
 // Link the nodes to form 1 <-> 2 <-> 3
+node0.next = node1;
+node1.prev = node0;
 node1.next = node2;
 node2.prev = node1;
 node2.next = node3;
 node3.prev = node2;
 
+console.log(node0);
 
-console.log(node1);
+var pairSum = function(head) {
 
-addNodeModified(node2, nodeK);
+    // Apply the fast and slow algorithm to find the node in the middle an its previousNode.
+    let slow = head;
+    let fast = head;
+    let curr = null;
+    let prevNode = null;
 
+    while (fast && fast.next) {
+        prevNode = slow;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    curr = slow;
+
+    console.log(prevNode);
+
+    // Revert the linked list from the middle node onwards and save a reference to the last node
+
+    while (curr) {
+        prevNode = slow.next
+        
+    }
+
+    
+    // Sum the twin nodes one by one to check which sum is the greates.
+    
+};
+
+pairSum(node0)
